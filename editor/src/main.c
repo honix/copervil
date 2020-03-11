b1f4ba6e100b0f48f671102c6d5ae34386f6c94c
Fyodor Shchukin <ted888@ya.ru>
2020-03-12 00:33:53 +0300

    some text

--- editor/src/main.c
+++ editor/src/main.c
@@ -4,9 +4,15 @@
 #define NANOVG_GL3_IMPLEMENTATION
 #include "../nanovg/src/nanovg_gl.h"
 
+#include "../../core/src/node.h"
+#include "../../core/src/utils.h"
+
 int main(void)
 {
     GLFWwindow* window;
+    struct NVGcontext* vg;
+
+    struct node *node;
 
     /* Initialize the library */
     if (!glfwInit())
@@ -35,11 +41,21 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
     
-    struct NVGcontext* vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES);
+    vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
     if (vg == NULL) {
 		printf("Could not init nanovg.\n");
 		return -1;
 	}
+
+    nvgCreateFont(vg, "sans", "../Roboto-Regular.ttf");
+
+    // glEnable(GL_BLEND);
+    // glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // glEnable(GL_CULL_FACE);
+    // glDisable(GL_DEPTH_TEST);
+    // end initialization
+
+    node = make_node("node_name", sum);
 
     int frame = 0;
 
@@ -50,15 +66,29 @@ int main(void)
 
         /* Render here */
         glViewport(0, 0, 512, 512);
-        glClearColor(0.1f, 0, 0, 1);
+        glClearColor(0.5f, 0.5f, 0.5f, 1);
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
 
         nvgBeginFrame(vg, 512, 512, 1);
 
-        nvgBeginPath(vg);
-        nvgRect(vg, 100 + (sin(frame * 0.1f) * 10),100, 120,30);
-        nvgFillColor(vg, nvgRGBA(255,192,0,255));
-        nvgFill(vg);
+        for (int i = 0; i < 15; i++) {
+            float x = 100 + i * 15;// + (sin(frame * 0.1f) * 10);
+            float y = 100 + i * 15;
+
+            nvgBeginPath(vg);
+            nvgRect(vg, x,y, 120,30);
+            nvgFillColor(vg, nvgRGBA(255,192,0,255));
+            nvgFill(vg);
+            nvgStroke(vg);
+
+            nvgFontSize(vg, 15.0f); 
+            nvgFontFace(vg, "sans");
+
+            nvgTextAlign(vg,NVG_ALIGN_LEFT|NVG_ALIGN_MIDDLE);
+
+            nvgFillColor(vg, nvgRGBA(0, 0, 0, 255));
+            nvgText(vg, x + 10, y + 30 / 2, node->name, NULL);
+        }
 
         nvgEndFrame(vg);
 
