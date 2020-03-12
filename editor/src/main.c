2a99bca1224b319a6ba269b4d4f46e8375764768
Fyodor Shchukin <ted888@ya.ru>
2020-03-12 14:59:37 +0300

    draw_node

--- editor/src/main.c
+++ editor/src/main.c
@@ -7,10 +7,46 @@
 #include "../../core/src/node.h"
 #include "../../core/src/utils.h"
 
+void draw_node(struct NVGcontext *vg, struct node *node, int x, int y) // TODO: x and y will be in node
+{
+    const pin_size = 10;
+    const pin_padding = 5;
+
+    float width = pin_padding + (pin_size + pin_padding) * 16;
+    const float height = 45;
+
+    nvgBeginPath(vg);
+    nvgRect(vg, x, y, width, height);
+    nvgFillColor(vg, nvgRGBA(255, 192, 0, 255));
+    nvgFill(vg);
+    nvgStroke(vg);
+
+    nvgFillColor(vg, nvgRGBA(0, 0, 0, 128));
+    for (int i = 0; i < 16; i++)
+    {
+        nvgBeginPath(vg);
+        nvgRect(vg, x + pin_padding + (pin_size + pin_padding) * i, y, pin_size, pin_size);
+        nvgFill(vg);
+    }
+
+    for (int i = 0; i < 16; i++)
+    {
+        nvgBeginPath(vg);
+        nvgRect(vg, x + pin_padding + (pin_size + pin_padding) * i, y + height - pin_size, pin_size, pin_size);
+        nvgFill(vg);
+    }
+
+    nvgFontSize(vg, 15.0f);
+    nvgFontFace(vg, "sans");
+    nvgTextAlign(vg, NVG_ALIGN_LEFT | NVG_ALIGN_MIDDLE);
+    nvgFillColor(vg, nvgRGBA(0, 0, 0, 255));
+    nvgText(vg, x + 10, y + height / 2, node->name, NULL);
+}
+
 int main(void)
 {
-    GLFWwindow* window;
-    struct NVGcontext* vg;
+    GLFWwindow *window;
+    struct NVGcontext *vg;
 
     struct node *node;
 
@@ -19,15 +55,15 @@ int main(void)
         return -1;
 
 #ifndef _WIN32 // don't require this on win32, and works with more cards
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #endif
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
 
 #ifdef DEMO_MSAA
-	glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_SAMPLES, 4);
 #endif
 
     /* Create a windowed mode window and its OpenGL context */
@@ -40,12 +76,13 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-    
+
     vg = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES | NVG_DEBUG);
-    if (vg == NULL) {
-		printf("Could not init nanovg.\n");
-		return -1;
-	}
+    if (vg == NULL)
+    {
+        printf("Could not init nanovg.\n");
+        return -1;
+    }
 
     nvgCreateFont(vg, "sans", "../Roboto-Regular.ttf");
 
@@ -67,27 +104,13 @@ int main(void)
         /* Render here */
         glViewport(0, 0, 512, 512);
         glClearColor(0.5f, 0.5f, 0.5f, 1);
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
 
         nvgBeginFrame(vg, 512, 512, 1);
 
-        for (int i = 0; i < 15; i++) {
-            float x = 100 + i * 15;// + (sin(frame * 0.1f) * 10);
-            float y = 100 + i * 15;
-
-            nvgBeginPath(vg);
-            nvgRect(vg, x,y, 120,30);
-            nvgFillColor(vg, nvgRGBA(255,192,0,255));
-            nvgFill(vg);
-            nvgStroke(vg);
-
-            nvgFontSize(vg, 15.0f); 
-            nvgFontFace(vg, "sans");
-
-            nvgTextAlign(vg,NVG_ALIGN_LEFT|NVG_ALIGN_MIDDLE);
-
-            nvgFillColor(vg, nvgRGBA(0, 0, 0, 255));
-            nvgText(vg, x + 10, y + 30 / 2, node->name, NULL);
+        for (int i = 0; i < 15; i++)
+        {
+            draw_node(vg, node, 100 + 15 * i, 100 + 15 * i);
         }
 
         nvgEndFrame(vg);
